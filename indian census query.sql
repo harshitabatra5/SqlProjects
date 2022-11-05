3f7892d4d93829f877fe8a3e6c285071829c4020
harshitabatra5 <91491504+harshitabatra5@users.noreply.github.com>
2022-11-05 14:25:47 +0530

    Update indian census query.sql

--- indian census query.sql
+++ indian census query.sql
@@ -1,10 +1,3 @@
-
-/* you can get these two datasets from the following links
-[dataset 1](https://docs.google.com/spreadsheets/d/1ML9R2hVbFj3FSGSZ6_eoDV6TSqsH6hEv/edit?usp=sharing&ouid=116155490114219473709&rtpof=true&sd=true)
-[dataset 2](https://docs.google.com/spreadsheets/d/1IWpzYcYGGmho7IZgriPMdkB620AAhxj_/edit?usp=sharing&ouid=116155490114219473709&rtpof=true&sd=true)
-*/
-
-
 select * from project.dbo.data1;
 select * from project.dbo.data2;
 
